eedbbf736bb4371a30ba814800d36e0d9b72ce1b
isaias-b <isaias-b@users.noreply.github.com>
2018-08-27 09:48:42 +0200

    Add remaining level methods to Logger

--- ets-logging-apisandbox/src/main/scala/de/kaufhof/ets/logging/stdout/Test.scala
+++ ets-logging-apisandbox/src/main/scala/de/kaufhof/ets/logging/stdout/Test.scala
@@ -90,8 +90,10 @@ object Api {
   }
   trait Logger[E, O] {
     def event(attrs: LogAttribute[E]*): O
+    def trace(msg: String, attrs: LogAttribute[E]*): O
     def debug(msg: String, attrs: LogAttribute[E]*): O
     def info(msg: String, attrs: LogAttribute[E]*): O
+    def warn(msg: String, attrs: LogAttribute[E]*): O
     def error(msg: String, attrs: LogAttribute[E]*): O
   }
 
@@ -316,14 +318,18 @@ object Api {
       with PredefKeysInstance[E]
       with LogAttributeProcessor[E, O] {
     def createLogger(cls: Class[_]): Logger = new Logger {
-      private val clsAttr = predefKeys.Logger -> cls
+      private lazy val clsAttr = predefKeys.Logger -> cls
       private def generic(attributes: Seq[LogAttribute[E]]): Output = process(attributes :+ clsAttr)
-      final def event(attrs: LogAttribute[E]*): Output = generic(attrs)
-      final def debug(msg: String, attrs: LogAttribute[E]*): Output =
+      final override def event(attrs: LogAttribute[E]*): Output = generic(attrs)
+      final override def trace(msg: String, attrs: LogAttribute[Encoded]*): Output =
+        generic(attrs :+ predefKeys.Message ~> msg :+ predefKeys.Level ~> Level.Trace)
+      final override def debug(msg: String, attrs: LogAttribute[E]*): Output =
         generic(attrs :+ predefKeys.Message ~> msg :+ predefKeys.Level ~> Level.Debug)
-      final def info(msg: String, attrs: LogAttribute[E]*): Output =
+      final override def info(msg: String, attrs: LogAttribute[E]*): Output =
         generic(attrs :+ predefKeys.Message ~> msg :+ predefKeys.Level ~> Level.Info)
-      final def error(msg: String, attrs: LogAttribute[E]*): Output =
+      final override def warn(msg: String, attrs: LogAttribute[Encoded]*): Output =
+        generic(attrs :+ predefKeys.Message ~> msg :+ predefKeys.Level ~> Level.Warn)
+      final override def error(msg: String, attrs: LogAttribute[E]*): Output =
         generic(attrs :+ predefKeys.Message ~> msg :+ predefKeys.Level ~> Level.Error)
     }
   }
